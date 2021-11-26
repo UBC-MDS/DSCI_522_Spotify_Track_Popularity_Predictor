eb8b7e4c90bdf363b25c059e365c6f760864e2bf
Victor Francis <francisvictor47@gmail.com>
2021-11-26 13:18:34 -0800

    Add bibliography and references

---
+++ src/clean_n_split.py
@@ -0,0 +1,44 @@
+# author : Group 27 
+# date : 2021-11-25
+
+""""Cleans data csv data from a data file audio_features.csv and save it to a local filepath as csv format"
+
+Usage: clean_n_split.py --file_path=<file_path> --out_file=<out_file>
+
+Options:
+--file_path=<file_path>   Path to data file for which to perform preprocessing on
+--out_file=<out_file>     Path (including filename) of where to locally write the train and test data
+"""
+
+import numpy as np
+import pandas as pd
+import os
+from docopt import docopt
+from sklearn.model_selection import cross_val_score, cross_validate, train_test_split
+
+
+opt = docopt(__doc__)
+
+def main(file_path, out_file):
+    data= pd.read_csv(file_path)
+
+    data = data.drop(columns = ["spotify_track_preview_url"])                   #drop the URL columns as it has so many NA and does not have useful information
+    data['spotify_genre'] = data['spotify_genre'].str[1:-1].str.replace("'","") # convert the spotify_genre to string 
+    data.iloc[:,3].replace("", np.NAN, inplace=True)                            #repalce the blank cells with NA
+    data = data.dropna()                                                        #drop the NA rows
+
+    train_df, test_df = train_test_split(data, test_size=0.2, random_state=123)
+    try:
+        train_df.to_csv(f'{out_file}/train_df.csv', index = False)
+    except:
+        os.makedirs(os.path.dirname(f'{out_file}/train_df.csv'))
+        data.to_csv(f'{out_file}/train_df.csv', index = False)
+    
+    try:
+        test_df.to_csv(f'{out_file}/test_df.csv', index = False)
+    except:
+        os.makedirs(os.path.dirname(f'{out_file}/test_df.csv'))
+        test_df.to_csv(f'{out_file}/test_df.csv', index = False)
+
+if __name__ == "__main__":
+    main(opt["--file_path"], opt["--out_file"])
---
+++ src/preprocess_n_model.py
@@ -0,0 +1,133 @@
+# author : Group 27 
+# date : 2021-11-25
+
+"""Performs some statistical or machine learning analysis and summarizes the results as a figure(s) and a table(s)
+Usage: preprocess_n_model.py --file_path=<file_path> --out_file=<out_file>
+
+Options:
+--file_path=<file_path>   Path to train processed data file for which to perform preprocessing on
+--out_file=<out_file>     Path (including filename) of where to locally write the file
+"""
+
+import altair as alt
+import numpy as np
+import pandas as pd
+import os
+from docopt import docopt
+from sklearn.compose import ColumnTransformer, make_column_transformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression, Ridge, RidgeCV
+from sklearn.preprocessing import (
+    OneHotEncoder,
+    OrdinalEncoder,
+    StandardScaler,
+)
+from sklearn.model_selection import (
+    GridSearchCV,
+    RandomizedSearchCV,
+    ShuffleSplit,
+    cross_val_score,
+    cross_validate,
+    train_test_split,
+)
+
+opt = docopt(__doc__)
+
+def main(file_path, out_file):
+    df_train = pd.read_csv(f'{file_path}/train_df.csv')
+    df_test = pd.read_csv(f'{file_path}/test_df.csv')
+    X_train, y_train = df_train.drop(columns=["spotify_track_popularity"]), df_train["spotify_track_popularity"]
+    X_test, y_test = df_test.drop(columns=["spotify_track_popularity"]), df_test["spotify_track_popularity"]
+    
+    numeric_features  = [
+        'spotify_track_duration_ms',
+        'danceability',
+        'energy','key',
+        'loudness','mode',
+        'speechiness',
+        'acousticness',
+        'instrumentalness',
+        'liveness',
+        'valence',
+        'tempo',
+        'time_signature']
+    categorical_features = ['performer']
+    binary_features = ['spotify_track_explicit']
+    drop_features = ['song_id', 'spotify_track_id', 'spotify_track_album']
+
+
+    preprocessor = make_column_transformer(
+        (StandardScaler(), numeric_features),
+        (OneHotEncoder(handle_unknown="ignore"), categorical_features),
+        (OneHotEncoder(drop='if_binary', handle_unknown="ignore"), binary_features),
+        (CountVectorizer(max_features = 20000, stop_words="english"), "spotify_genre"),
+        (CountVectorizer(max_features = 20000, stop_words="english"), "song"),
+        ("drop", drop_features)
+    )
+
+    pipe = make_pipeline(preprocessor, Ridge())
+    cv_df = pd.DataFrame(cross_validate(pipe, X_train, y_train, cv=10, return_train_score=True))
+
+    cv_df.to_csv(f'{out_file}/cv_df.csv', index = False)    # save the cv file 
+    
+
+    #random seach hyperparameters
+    
+    param_grid = {
+        "ridge__alpha": np.logspace(-3,2,6),
+        "columntransformer__countvectorizer-1__binary": np.array([True, False]),
+        "columntransformer__countvectorizer-1__max_features": np.arange(1000, 10000, 20000),
+        "columntransformer__countvectorizer-2__binary": np.array([True, False]),
+        "columntransformer__countvectorizer-2__max_features": np.arange(1000, 10000, 20000)
+    }
+
+    random_search = RandomizedSearchCV(
+        pipe, param_distributions=param_grid, n_jobs=-1, n_iter=10, cv=5
+    )
+    
+    random_search.fit(X_train, y_train)
+
+    random_search_results = pd.DataFrame(random_search.cv_results_)[
+        [
+            "mean_test_score",
+            "param_ridge__alpha",
+            "param_columntransformer__countvectorizer-1__max_features",
+            "param_columntransformer__countvectorizer-1__binary",
+            "param_columntransformer__countvectorizer-2__max_features",
+            "param_columntransformer__countvectorizer-2__binary",
+            "rank_test_score",
+        ]
+    ].set_index("rank_test_score").sort_index()
+  
+    random_search_results.to_csv(f'{out_file}/best_hyperparameters.csv', index = False)   # save the random_search results
+
+    #Evaluating on the test set 
+    y_predicted = random_search.predict(X_test)
+    df = pd.DataFrame({'y_test':y_test, 'y_predicted':y_predicted})
+
+    plot = alt.Chart(df, title= "Predicted versus true Spotify popularities").mark_point(filled=True, clip=True).encode(
+        alt.X('y_test', title='Predicted values of Spotify popularities'),
+        alt.Y('y_predicted', title='True values of Spotify popularities', scale=alt.Scale(domain=(0, 100)))
+    )
+    plot_2 = plot + plot.mark_line(color = 'black').encode(
+        alt.Y('y_test')
+    )
+    plot_2.save(f'{out_file}/predict_vs_test.png')   #needs altair_saver package
+
+if __name__ == "__main__":
+  main(opt["--file_path"], opt["--out_file"])
+
+
+
+
+
+
+
+
+
+
+
+
+
